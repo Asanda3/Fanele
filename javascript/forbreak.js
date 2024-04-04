43c8f59ec03713f31b4b8e30eb608d9e5ee1d13d
Asanda3 <fanele351@gmail.com>
2024-04-04 15:46:58 +0200

    For break loop

---
+++ javascript/forbreak.js
@@ -0,0 +1,9 @@
+
+//create a for loop with break statement to limit the increament
+for(i=1; i++;){
+    //break conditon
+    if(i==7){
+        break;
+    }
+    console.log(i)
+}
---
+++ javascript/forloops.js
@@ -0,0 +1,23 @@
+let courses =["JavaScript", "Java" ,"Angular", "React"];
+
+let i; //counter fot the for-loop
+let length = courses.length; //total number of courses(Arrays)
+let faculty = courses.toString;
+let len = faculty.length;
+
+let space= ' ';  //home made space
+
+for(i=0; i<length; i++){
+    courses[i] += space
+    
+}
+    console.log(courses)
+
+//lets display text five times accourding to the number of days in a week
+const n=5;
+for(i=1; i<=n; i++){
+
+    console.log(`lets code in JavaScript ` + i + ' Days of a week')
+
+}
+
